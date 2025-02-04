9e449ca958afdfd7cabd689730024bd38aed7a59
Juma186 <j2131833@gmail.com>
2025-02-04 11:18:22 +0300

    new commit

---
+++ Variables.py
@@ -0,0 +1,20 @@
+#Variable is a container used to store a value
+
+number = 10
+firstname = "Mathew"
+lastname= "Juma"
+language ="Python"
+a = 10
+b = 15
+
+
+
+print (number)
+print (firstname)
+print(lastname)
+print(firstname, lastname)
+print (language, "is a web programming language")
+print(a+b)
+print("The sum of a+b is ", a+b)
+
+# The sum of 10 and 15 is 25
---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display a value/output
+
+print("Juma")
+print("eMobilis")
+print(39)
+print(100*2)

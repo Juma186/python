db7de557158b51052e524e8daaaddd685e898fd8
Juma186 <j2131833@gmail.com>
2025-02-04 13:15:31 +0300

    second commit

--- Variables.py
+++ Variables.py
@@ -15,6 +15,6 @@ print(lastname)
 print(firstname, lastname)
 print (language, "is a web programming language")
 print(a+b)
-print("The sum of a+b is ", a+b)
+print("The sum of ", a, "and" ,b, "is",a+b)
 
 # The sum of 10 and 15 is 25
---
+++ datatype.py
@@ -0,0 +1,33 @@
+
+number = 45 # Int
+num = 66.78 # Float
+text = "Hello" # Str
+isMammal = True  # Boolean
+
+# DataStructures
+cars = ["Toyota", "Mercedes", "Ford"] #List
+fruits = ( "banana", "orange", "strawberry") # Tuple
+countries = {"England", "France", "Germany"} # Set
+details = {
+    "firstname" : "Brian",
+    "course" : "MIT",
+    "Age": 19,
+    "maritalstatus": "Single"
+}
+
+
+
+
+
+print  (number)
+print  (num)
+print  (text)
+print (isMammal)
+print (cars)
+print (fruits)
+print (details)
+print(details["course"])
+
+# Typecasting
+print(float (number))
+print(int(number))
